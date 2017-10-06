3eb91388b0d01406b83a45537510d1232de3c939
9Kevinzhao <9kzhao@stpatrick.org>
2017-10-06 14:50:56 -0500

    Add files via upload

--- testsort.js
+++ testsort.js
@@ -1,9 +1,10 @@
 const Sort=require('./Sort.js');
 
 function main(){
-  let list=[1,3,2,4,5,7,6,8]
+  let list=[1,3,2,4,5,7,6,1]
   console.log(Sort.selection(list));
   console.log(Sort.insertion(list));
+  console.log(Sort.merge(list));
 
 }
 
